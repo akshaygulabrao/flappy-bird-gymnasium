7ffb7148571defe6ce9968ed7a95fd674d630a16
Akshay Gulabrao <aksgula22@gmail.com>
2024-12-25 20:44:46 -0800

    fix: normalize=True now works as expected
    Test-perfect previously had a bug because the agent's state recognition
    was not normalized correctly when normalize=True. I verified that it now
    works correctly because it achieved an avg score of 1000 when being run for
    10 runs. Anything above 750 would prove that test-perfect runs correctly

--- flappy_bird_gymnasium/tests/test_perfect.py
+++ flappy_bird_gymnasium/tests/test_perfect.py
@@ -45,14 +45,18 @@ def agent(obs,normalize=False):
             return 1
         return 0
     else:
+        pipe = 0
         if obs[0] < 5/288:
             pipe = 1
-        x = obs[pipe *3]/288
-        bot = obs[pipe * 3 + 2]/512
-        top = obs[pipe * 3 + 1]/512
-        y_next = obs[-3] + obs[-2] + 24 + 1
-        if 74/288 < x < 88/288 and obs[-3] - 45 >= top:
-            return 1
+        x = obs[pipe *3]
+        bot = obs[pipe * 3 + 2]
+        top = obs[pipe * 3 + 1]
+   
+        y_next = obs[-3] + (obs[-2]*10) / 512 # current y + current y_velocity
+        y_next += 1/512 # 1 pixel acceleration per frame
+        y_next += 24/512 # height of bird
+        if 72/288 < x < 88/288 and obs[-3] - 45/512 >= top:
+                return 1
         elif y_next >= bot:
             return 1
         return 0
@@ -83,6 +87,9 @@ def play(use_lidar=True,render_mode="human",normalize=True):
     return info['score']
 
 if __name__ == "__main__":
-    for i in range(100):
-        score = play(use_lidar=False,render_mode=None,normalize=False)
-        print(f"Score: {score}")
+
+    scores = []
+    for i in range(10):
+        score = play(use_lidar=False,render_mode=None,normalize=True)
+        scores.append(score)
+    print(f"Average score (normalize=True): {sum(scores)/len(scores)}")
